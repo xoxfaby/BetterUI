e86f25f519bbabe221e42138b9cb9ea549dbdea4
XoXFaby <xoxfaby@gmail.com>
2020-09-07 22:57:40 +0200

    Initial commit

---
+++ BetterUI/BetterUI.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using System.Collections.Generic;
+
+using RoR2;
+using RoR2.UI;
+using BepInEx;
+
+
+namespace BetterUI
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.xoxfaby.BetterUI", "BetterUI", "1.0.0")]
+
+
+    public class BetterUI : BaseUnityPlugin
+    {
+
+        public static BindingFlags flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+
+        private ConfigManager config;
+        public void Awake()
+        {
+            config = new ConfigManager(this);
+
+            if (config.showHidden)
+            {
+                On.RoR2.UI.ItemInventoryDisplay.ItemIsVisible += hook_ItemIsVisible;
+            }
+
+            if (config.AdvancedDescriptions)
+            {
+                On.RoR2.UI.GenericNotification.SetItem += hook_SetItem;
+                On.RoR2.UI.ItemIcon.SetItemIndex += hook_SetItemIndex;
+            }
+
+            if (config.sortItems)
+            { 
+                On.RoR2.UI.ItemInventoryDisplay.OnInventoryChanged += hook_OnInventoryChanged;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (config.showHidden)
+            {
+                On.RoR2.UI.ItemInventoryDisplay.ItemIsVisible -= hook_ItemIsVisible;
+            }
+
+            if (config.AdvancedDescriptions)
+            {
+                On.RoR2.UI.GenericNotification.SetItem -= hook_SetItem;
+                On.RoR2.UI.ItemIcon.SetItemIndex -= hook_SetItemIndex;
+            }
+
+            if (config.sortItems)
+            {
+                On.RoR2.UI.ItemInventoryDisplay.OnInventoryChanged -= hook_OnInventoryChanged;
+            }
+        }
+
+
+        private bool hook_ItemIsVisible(On.RoR2.UI.ItemInventoryDisplay.orig_ItemIsVisible orig, ItemIndex itemIndex)
+        {
+            return true;
+        }
+
+        private void hook_SetItem(On.RoR2.UI.GenericNotification.orig_SetItem orig, RoR2.UI.GenericNotification self, ItemDef itemDef)
+        {
+            orig(self, itemDef);
+
+            self.descriptionText.token = itemDef.descriptionToken;
+        }
+
+        private void hook_SetItemIndex(On.RoR2.UI.ItemIcon.orig_SetItemIndex orig, RoR2.UI.ItemIcon self, ItemIndex itemIndex, int itemCount)
+        {
+            orig(self, itemIndex, itemCount);
+
+            self.tooltipProvider.bodyToken = ItemCatalog.GetItemDef(itemIndex).descriptionToken;
+        }
+        private void hook_OnInventoryChanged(On.RoR2.UI.ItemInventoryDisplay.orig_OnInventoryChanged orig, RoR2.UI.ItemInventoryDisplay self)
+        {
+
+            orig(self);
+
+            Inventory inventory = typeof(ItemInventoryDisplay).GetField("inventory", flags).GetValue(self) as Inventory;
+            if (inventory)
+            {
+                if (!inventory.itemAcquisitionOrder.Any())
+                {
+                    System.Console.WriteLine("no any");
+                    return;
+                }
+                IOrderedEnumerable<ItemIndex> finalOrder = inventory.itemAcquisitionOrder.OrderBy(a => 1);
+                foreach (char c in config.sortOrder.ToCharArray())
+                {
+                    switch (c)
+                    {
+                        case '0': // Tier Ascending
+                            finalOrder = finalOrder.ThenBy(item => config.tierOrder[(int)ItemCatalog.GetItemDef(item).tier]);
+                            break;
+                        case '1': // Tier Descending
+                            finalOrder = finalOrder.ThenByDescending(item => config.tierOrder[(int)ItemCatalog.GetItemDef(item).tier]);
+                            break;
+                        case '2': // Stack Size Ascending
+                        {
+                            int[] itemStacks = (int[])typeof(ItemInventoryDisplay).GetField("itemStacks", flags).GetValue(self);
+                            finalOrder = finalOrder.ThenBy(item => itemStacks[(int)item]);
+                            break;
+                        }
+                        case '3': // Stack Size Descending
+                            {
+                                int[] itemStacks = (int[])typeof(ItemInventoryDisplay).GetField("itemStacks", flags).GetValue(self);
+                            finalOrder = finalOrder.ThenByDescending(item => itemStacks[(int)item]);
+                            break;
+                        }
+                        case '4': // Pickup Order
+                            finalOrder = finalOrder.ThenBy(item => inventory.itemAcquisitionOrder.IndexOf(item));
+                            break;
+                        case '5': // Pickup Order Reversed
+                            finalOrder = finalOrder.ThenByDescending(item => inventory.itemAcquisitionOrder.IndexOf(item));
+                            break;
+                        case '6': // Alphabetical
+                            finalOrder = finalOrder.ThenBy(item => Language.GetString(ItemCatalog.GetItemDef(item).nameToken));
+                            break;
+                        case '7': // Alphabetical Reversed
+                            finalOrder = finalOrder.ThenByDescending(item => Language.GetString(ItemCatalog.GetItemDef(item).nameToken));
+                            break;
+                        case '8': // Random"
+                            Random random = new Random();
+                            finalOrder = finalOrder.ThenBy(item => random.Next());
+                            break;
+                        case 's': // Scrap First
+                            finalOrder = finalOrder.ThenByDescending(item => ItemCatalog.GetItemDef(item).ContainsTag(ItemTag.Scrap));
+                            break;
+                        case 'S': // Scrap Last
+                            finalOrder = finalOrder.ThenByDescending(item => ItemCatalog.GetItemDef(item).DoesNotContainTag(ItemTag.Scrap));
+                            break;
+                        case 'd': // Damage First
+                            finalOrder = finalOrder.ThenByDescending(item => ItemCatalog.GetItemDef(item).ContainsTag(ItemTag.Damage));
+                            break;
+                        case 'D': // Damage Last
+                            finalOrder = finalOrder.ThenByDescending(item => ItemCatalog.GetItemDef(item).DoesNotContainTag(ItemTag.Damage));
+                            break;
+                        case 'h': // Healing First
+                            finalOrder = finalOrder.ThenByDescending(item => ItemCatalog.GetItemDef(item).ContainsTag(ItemTag.Healing));
+                            break;
+                        case 'H': // Healing Last
+                            finalOrder = finalOrder.ThenByDescending(item => ItemCatalog.GetItemDef(item).DoesNotContainTag(ItemTag.Healing));
+                            break;
+                        case 'u': // Utility First
+                            finalOrder = finalOrder.ThenByDescending(item => ItemCatalog.GetItemDef(item).ContainsTag(ItemTag.Utility));
+                            break;
+                        case 'U': // Utility Last
+                            finalOrder = finalOrder.ThenByDescending(item => ItemCatalog.GetItemDef(item).DoesNotContainTag(ItemTag.Utility));
+                            break;
+                        case 'o': // OnKillEffect First
+                            finalOrder = finalOrder.ThenByDescending(item => ItemCatalog.GetItemDef(item).ContainsTag(ItemTag.OnKillEffect));
+                            break;
+                        case 'O': // OnKillEffect Last
+                            finalOrder = finalOrder.ThenByDescending(item => ItemCatalog.GetItemDef(item).DoesNotContainTag(ItemTag.OnKillEffect));
+                            break;
+                        case 'e': // EquipmentRelated First
+                            finalOrder = finalOrder.ThenByDescending(item => ItemCatalog.GetItemDef(item).ContainsTag(ItemTag.EquipmentRelated));
+                            break;
+                        case 'E': // EquipmentRelated Last
+                            finalOrder = finalOrder.ThenByDescending(item => ItemCatalog.GetItemDef(item).DoesNotContainTag(ItemTag.EquipmentRelated));
+                            break;
+                        case 'p': // SprintRelated First
+                            finalOrder = finalOrder.ThenByDescending(item => ItemCatalog.GetItemDef(item).ContainsTag(ItemTag.SprintRelated));
+                            break;
+                        case 'P': // SprintRelated Last
+                            finalOrder = finalOrder.ThenByDescending(item => ItemCatalog.GetItemDef(item).DoesNotContainTag(ItemTag.SprintRelated));
+                            break;
+                    }
+                }
+
+
+                ItemIndex[] itemOrder = (ItemIndex[])typeof(ItemInventoryDisplay).GetField("itemOrder", flags).GetValue(self);
+                if (itemOrder != null)
+                {
+                    finalOrder.ToList().CopyTo(itemOrder);
+                }
+            }
+        }
+    }
+}
---
+++ BetterUI/ConfigManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using BepInEx;
+
+namespace BetterUI
+{
+    class ConfigManager
+    {
+        private static BepInEx.Configuration.ConfigEntry<bool> configAdvancedDescriptions;
+        public bool AdvancedDescriptions { get => configAdvancedDescriptions.Value; }
+
+
+        private static BepInEx.Configuration.ConfigEntry<bool> configShowHidden;
+        public bool showHidden { get => configShowHidden.Value; }
+
+
+        private static BepInEx.Configuration.ConfigEntry<bool> configSortItems;
+        public bool sortItems { get => configSortItems.Value; }
+
+        private static BepInEx.Configuration.ConfigEntry<String> configTierOrder;
+        public int[] tierOrder { get => new int[]
+            {
+            configTierOrder.Value.IndexOf('0'),
+            configTierOrder.Value.IndexOf('1'),
+            configTierOrder.Value.IndexOf('2'),
+            configTierOrder.Value.IndexOf('3'),
+            configTierOrder.Value.IndexOf('4'),
+            configTierOrder.Value.IndexOf('5'),
+            };
+        }
+
+
+        private static BepInEx.Configuration.ConfigEntry<String> configSortOrder;
+        public string sortOrder { get => configSortOrder.Value; }
+
+
+
+
+        public ConfigManager(BetterUI mod)
+        {
+            configAdvancedDescriptions = mod.Config.Bind<bool>("BetterUI", "AdvancedDescriptions", true,
+                "Show advanced descriptions when hovering over an item or picking it up.");
+
+            configShowHidden = mod.Config.Bind<bool>("BetterUI", "ShowHidden", false,
+                "Show hidden items in the item inventory");
+
+            configSortItems = mod.Config.Bind<bool>("BetterUI", "SortItems", true,
+                "Sort items");
+
+            configTierOrder = mod.Config.Bind<String>("Sorting", "TierOrder", "012345",
+                "Tiers in ascending order, left to right \n0 = White, 1 = Green, 2 = Red, 3 = Lunar, 4 = Boss, 5 = NoTier");
+
+            configSortOrder = mod.Config.Bind<String>("Sorting", "SortOrder", "S134",
+                "What to sort the items by, most important to least important.\n" +
+                "0 = Tier Ascending\n" +
+                "1 = Tier Descending\n" +
+                "2 = Stack Size Ascending\n" +
+                "3 = Stack Size Descending\n" +
+                "4 = Pickup Order\n" +
+                "5 = Pickup Order Reversed\n" +
+                "6 = Alphabetical\n" +
+                "7 = Alphabetical Reversed\n" +
+                "8 = Random\n\n" +
+                "Tag Based:\n" +
+                "s = Scrap First\n" +
+                "S = Scrap Last\n" +
+                "d = Damage First\n" +
+                "D = Damage Last\n" +
+                "h = Healing First\n" +
+                "H = Healing Last\n" +
+                "u = Utility First\n" +
+                "U = Utility Last\n" +
+                "o = On Kill Effect First\n" +
+                "O = On Kill Effect Last\n" +
+                "e = Equipment Related First\n" +
+                "E = Equipment Related Last\n" +
+                "p = Sprint Related First\n" +
+                "P = Sprint Related Last\n");
+        }
+    }
+}
