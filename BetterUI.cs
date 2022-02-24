1ce662b5aca48845da9fcfc61d5c0a6b07731a43
xoxfaby <xoxfaby@gmail.com>
2022-02-24 04:22:59 +0100

    Fix deps

--- BetterUI.cs
+++ BetterUI.cs
@@ -12,7 +12,7 @@ namespace BetterUI
     [BepInDependency("dev.ontrigger.itemstats", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency("com.xoxfaby.BetterAPI", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInPlugin("com.xoxfaby.BetterUI", "BetterUI", "2.4.0.3")]
-    public class BetterUIPlugin : BetterAPI.BetterUnityPlugin<BetterUIPlugin>
+    public class BetterUIPlugin : BetterUnityPlugin.BetterUnityPlugin<BetterUIPlugin>
     {
 
         internal delegate void HUDAwakeEvent(RoR2.UI.HUD self);
