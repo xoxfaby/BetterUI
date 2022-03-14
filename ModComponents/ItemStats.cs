7cd1bab8182b063af1d7596065dbd5b3ca53e805
xoxfaby <xoxfaby@gmail.com>
2022-03-14 19:28:31 +0100

    Create ItemStats.cs

---
+++ ModComponents/ItemStats.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RoR2;
+
+namespace BetterUI
+{
+    public static class ItemStats
+    {
+        public static void RegisterItemStat(ItemDef itemDef, )
+        {
+
+        }
+
+        public static void RegisterModifier(ItemDef itemDef, )
+        {
+
+        }
+
+        public static void RegisterTag(ItemDef itemDef, )
+        {
+
+        }
+    }
+}
