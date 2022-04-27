0eb64cda1a9f89a0a03c2e63d970fd6f9ce81b1f
XoXFaby <xoxfaby@gmail.com>
2022-04-27 12:17:28 +0200

    add editortools and copypath

---
+++ Unity/Assets/Editor/CopyPath.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BetterUI.EditorTools
+{
+    public static class CopyGameObjectPathFromHierarchy
+    {
+        [MenuItem("GameObject/Copy Path", false, 11)]
+        static void CopyPath()
+        {
+            GameObject currentGameObject = Selection.activeGameObject;
+
+            if (currentGameObject == null)
+                return;
+
+            string path = currentGameObject.name;
+
+            while (currentGameObject.transform.parent != null)
+            {
+                currentGameObject = currentGameObject.transform.parent.gameObject;
+
+                path = $"{currentGameObject.name}/{path}";
+            }
+
+            EditorGUIUtility.systemCopyBuffer = path;
+        }
+
+        /// <summary>
+        /// Only allow path copying if 1 object is selected.
+        /// </summary>
+        [MenuItem("GameObject/Copy Path", true)]
+        static bool CopyPathValidation() => Selection.gameObjects.Length == 1;
+    }
+}
