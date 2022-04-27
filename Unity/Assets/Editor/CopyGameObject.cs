ecb5b69304c61fa31577ca02f9975b11efd3a2c7
XoXFaby <xoxfaby@gmail.com>
2022-04-27 22:10:34 +0200

    add copy gameobject editor method

---
+++ Unity/Assets/Editor/CopyGameObject.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BetterUI.EditorTools
+{
+    public static class CopyGameObject
+    {
+        [MenuItem("GameObject/Duplicate GameObject", false, 11)]
+        static void DuplicateGameObject()
+        {
+            CloneGameObject(Selection.activeGameObject);
+        }
+
+        /// <summary>
+        /// Only allow path copying if 1 object is selected.
+        /// </summary>
+        [MenuItem("GameObject / Duplicate GameObject", true)]
+        static bool DuplicateGameObjectValidation() => Selection.gameObjects.Length == 1;
+
+        public static GameObject CloneGameObject(GameObject originalObject, GameObject parent = null)
+        {
+            var newGameObject = new GameObject(originalObject.name);
+            newGameObject.transform.parent = parent?.transform;
+
+            foreach (Transform originalChildObject in originalObject.transform)
+            {
+                CloneGameObject(originalChildObject.gameObject, newGameObject);
+            }
+
+            foreach (var component in originalObject.GetComponents<Component>())
+            {
+                if (UnityEditorInternal.ComponentUtility.CopyComponent(component))
+                {
+                    UnityEditorInternal.ComponentUtility.PasteComponentAsNew(newGameObject);
+                }
+            }
+
+            return newGameObject;
+        }
+    }
+}
